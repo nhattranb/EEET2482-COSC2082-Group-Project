a6f707e478c1900fa8daa865c3227060edb7d113
toan396 <138806220+toan396@users.noreply.github.com>
2023-09-21 17:19:01 +0700

    Update rent.cpp

--- class/rent/rent.cpp
+++ class/rent/rent.cpp
@@ -14,53 +14,47 @@
 */
 
 #include <iostream>
-#include <fstream>
-#include <cstring>
-#include <iomanip>
 #include <vector>
-#include <string>
-#include <sstream>
-#include "rent.h"
 #include "motorbike.h"
 #include "customer.h"
 using namespace std;
 
 void requestToRent(Motorbike &motorbike, Customer &member) {
-            if (motorbike.available = false) {
-                cout << "Sorry, this motorbike is already rented. Please try again" << endl;
-            } else {
-                motorbike.available = true;
-                motorbike.renterName = member.name;
-                member.requests.push_back(motorbike.name);
-                cout << "Your request to rent " << motorbike.name << " has been sent." << endl;
-            }
-        }
+    if (!motorbike.isAvailable) {
+        cout << "Sorry, this motorbike is already rented. Please try again" << endl;
+    } else {
+        motorbike.isAvailable = false;
+        motorbike.renterName = member.name;
+        member.requests.push_back(motorbike.name);
+        cout << "Your request to rent " << motorbike.name << " has been sent." << endl;
+    }
+}
 
-void viewRequests() {
-            if (requests.empty()) {
-                cout << "Sorry but you have NO requests at the moment." << endl;
-            } else {
-                cout << "Here are your requests:" << endl;
-                for (int i = 0; i < requests.size(); i++) {
-                    cout << i + 1 << ". " << requests[i] << endl;
-                }
-            }
+void viewRequests(const vector<string> &requests) {
+    if (requests.empty()) {
+        cout << "Sorry but you have NO requests at the moment." << endl;
+    } else {
+        cout << "Here are your requests:" << endl;
+        for (int i = 0; i < requests.size(); i++) {
+            cout << i + 1 << ". " << requests[i] << endl;
         }
+    }
+}
 
-void acceptRequest(int index) {
-            if (index < 1 || index > requests.size()) {
-                cout << "Invalid request index." << endl;
+void acceptRequest(vector<Motorbike> &motorbikes, const string &name, vector<string> &requests, int index) {
+    if (index < 1 || index > requests.size()) {
+        cout << "Invalid request index." << endl;
+    } else {
+        for (int i = 0; i < motorbikes.size(); i++) {
+            if (motorbikes[i].name == requests[index - 1]) {
+                motorbikes[i].isAvailable = false;
+                motorbikes[i].renterName = name;
             } else {
-                for (int i = 0; i < motorbikes.size(); i++) {
-                    if (motorbikes[i].name == requests[index - 1]) {
-                        motorbikes[i].isRented = true;
-                        motorbikes[i].renterName = name;
-                    } else {
-                        motorbikes[i].isRented = false;
-                        motorbikes[i].renterName = "";
-                    }
-                }
-                cout << "You have accepted the request to rent " << requests[index - 1] << "." << endl;
-                requests.clear();
+                motorbikes[i].isAvailable = true;
+                motorbikes[i].renterName = "";
             }
         }
+        cout << "You have accepted the request to rent " << requests[index - 1] << "." << endl;
+        requests.clear();
+    }
+}
